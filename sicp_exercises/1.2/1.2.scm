e024077988882ce07566c7c321d6bc52867010c6
Paul Haddad <paulh16@gmail.com>
2019-02-25 07:02:39 -0500

    Add functions for 1.2.2 deep dive

--- sicp_exercises/1.2/1.2.scm
+++ sicp_exercises/1.2/1.2.scm
@@ -1,3 +1,27 @@
+;;; 1.2.2: Tree Recursion
+
+;;; Fibonacci - Recursive
+
+(define (fib-rec n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib-rec (- n 1))
+                 (fib-rec (- n 2))))))
+
+(fib-rec 5) ; 5
+
+;;; Fibonacci - Iterative
+
+(define (fib-it n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
+(fib-it 5) ; 5
+
 ;;; Counting Change
 
 (define (count-change amount)
