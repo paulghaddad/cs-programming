0aa87ca684fa6fe8bc899e90368858d3e7149eb2
Paul Haddad <paulh16@gmail.com>
2019-02-27 07:38:53 -0500

    Ex. 1.16

--- sicp_exercises/1.2/1.16.scm
+++ sicp_exercises/1.2/1.16.scm
@@ -11,14 +11,35 @@
 ;;; unchanged from state to state is a powerful way to think about the design of
 ;;; iterative algorithms.)
 
+;;; The key to this problem is keeping calls to expt-log-iter on the outside,
+;;; not as a subexpression as in the original iterative solution, ie. (* b
+;;; (fast-expt b (- n 1))
+
+;;; We achieve this with an additional state variable, recognizing the state
+;;; transformations as follows, and with the use of a quantity that is
+;;; invariant: ab**n
+
+;;; If even, this quantity has the same value, but the values it consists of
+;;; change: ab**n -> a(b**2)b**n/2
+;;; and if odd: ab**n -> abb**(- n 1)
+
+;;; when n == 0, b**0 = 1 and a by definition equals ab**n -> ab**nb**0
+
 (define (expt b n)
   (expt-log-iter b n 1))
 
 (define (expt-log-iter b n a)
+  (newline)
   (display n)
-  (cond ((= n 1) a)
-      ((even? n) (expt-log-iter b (/ n 2) (* a (square b))))
-      (else (* b (expt-log-iter b (- n 1) (* a b))))))
+  (newline)
+  (display b)
+  ; (newline)
+  ; (display a)
 
-(expt 2 8)
-(expt 2 9)
+  (cond ((= n 0) a)
+      ((even? n) (expt-log-iter (square b) (/ n 2) a))
+      (else (expt-log-iter b (- n 1) (* a b)))))
+
+(expt 2 0) ; 1
+(expt 2 8) ; 256
+(expt 2 9) ; 512
