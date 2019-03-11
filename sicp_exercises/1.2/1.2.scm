084cf9d2c71a030e9efdee0a84df57f3787312e6
Paul Haddad <paulh16@gmail.com>
2019-03-11 07:26:36 -0400

    SICP section 1.2.6 and Ex. 1.21

--- sicp_exercises/1.2/1.2.scm
+++ sicp_exercises/1.2/1.2.scm
@@ -85,3 +85,42 @@
     (gcd b (remainder a b))))
 
 (gcd 16 28)
+
+;;; Searching for Divisors
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+;;; Determine if prime
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+;;; Fermat Test
+
+(define (expmod base exp m)
+  (cond ((= exp ) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+          (remainder (* base (expmod base (- exp 1) m))
+                     m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= timex 0) true)
+        ((fermat-test n) (fast-prime? 1 (- times 1)))
+        (else false)))
